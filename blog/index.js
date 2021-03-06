158df0d9419ecabdbd4c0534dc4d45bb90fdf523
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-06 12:37:25 +0600

    task 4.2 separate routes and models

--- blog/index.js
+++ blog/index.js
@@ -1,18 +1,11 @@
+require("dotenv").config()
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/notes')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI;
 mongoose
@@ -26,24 +19,9 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/blogs", blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 const PORT = config.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
