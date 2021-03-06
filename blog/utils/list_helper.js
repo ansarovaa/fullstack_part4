3df21e61c2819d81df4d46d5de042e85beaf454d
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-06 13:13:34 +0600

    task 4.4

--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blog) => {
+    let total = blog.reduce(function(sum, likes) {
+        return sum + likes.likes
+    }, 0)
+    return total
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
