ab2087eb95337a0cc7769fa572bfb47483ab4b1c
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-08 11:10:23 +0600

    update post method for blog

--- blog/controllers/notes.js
+++ blog/controllers/notes.js
@@ -17,16 +17,15 @@ blogsRouter.get('/:id', async(request, response) => {
     }
 })
 
-blogsRouter.post("/", (request, response) => {
-    const blog = new Blog(request.body);
-
+blogsRouter.post("/", (request, response, next) => {
+    const body = request.body
+    const blog = new Blog({title: body.title, author: body.author, url: body.url, likes: body.likes})
     blog
         .save()
-        .then((result) => {
-            response
-                .status(201)
-                .json(result);
-        });
+        .then(savedBlog => {
+            response.json(savedBlog.toJSON())
+        })
+        .catch(error => next(error))
 });
 
 module.exports = blogsRouter;
