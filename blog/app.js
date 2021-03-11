5d54e0744b73f0412540f55e61022f90d262436b
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-11 09:55:51 +0600

    task 4.15

--- blog/app.js
+++ blog/app.js
@@ -6,7 +6,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
+const usersRouter = require('./controllers/user')
 const mongoUrl = config.MONGODB_URI;
 mongoose
     .connect(mongoUrl, {
@@ -22,6 +22,8 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/users", usersRouter)
 app.use("/api/blogs", blogsRouter)
+
 
 module.exports = app
