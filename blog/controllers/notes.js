c7146c97929c315a1543ac5ebfc7ff5b8d2e82d5
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-11 16:40:17 +0600

    task 4.17

--- blog/controllers/notes.js
+++ blog/controllers/notes.js
@@ -1,9 +1,15 @@
 const blogsRouter = require("express").Router();
 const {response} = require("express");
 const Blog = require("../models/blogs");
+const User = require('../models/users')
 
 blogsRouter.get('/', async(request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', {
+            username: 1,
+            name: 1
+        })
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
@@ -20,6 +26,7 @@ blogsRouter.get('/:id', async(request, response) => {
 
 blogsRouter.post("/", async(request, response, next) => {
     const body = request.body
+    const user = await User.findById(body.userId)
     if (!body.title || !body.url) {
         response
             .status(400)
@@ -29,9 +36,14 @@ blogsRouter.post("/", async(request, response, next) => {
             title: body.title,
             author: body.author,
             url: body.url,
-            likes: body.likes || 0
+            likes: body.likes || 0,
+            user: user._id
         })
         const savedBlog = await blog.save()
+        user.blogs = user
+            .blogs
+            .concat(savedBlog._id)
+        await user.save()
         response.json(savedBlog.toJSON())
     }
 
