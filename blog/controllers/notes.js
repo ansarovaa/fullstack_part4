1829a5d39394b6b2cec62c61338a0f1e23167289
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-11 17:10:00 +0600

    task 4.19

--- blog/controllers/notes.js
+++ blog/controllers/notes.js
@@ -1,7 +1,16 @@
 const blogsRouter = require("express").Router();
 const {response} = require("express");
+const jwt = require('jsonwebtoken')
 const Blog = require("../models/blogs");
 const User = require('../models/users')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async(request, response) => {
     const blogs = await Blog
@@ -26,7 +35,15 @@ blogsRouter.get('/:id', async(request, response) => {
 
 blogsRouter.post("/", async(request, response, next) => {
     const body = request.body
-    const user = await User.findById(body.userId)
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response
+            .status(401)
+            .json({error: 'token missing or invalid'})
+    }
+    const user = await User.findById(decodedToken.id)
+  
     if (!body.title || !body.url) {
         response
             .status(400)
