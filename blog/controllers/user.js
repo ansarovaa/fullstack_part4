40848966226ee33819e8eeab2de46028d9a0e1b0
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-11 13:56:09 +0600

    task 4.16

--- blog/controllers/user.js
+++ blog/controllers/user.js
@@ -10,6 +10,33 @@ usersRouter.get('/', async(request, response) => {
 usersRouter.post('/', async(request, response) => {
     const body = request.body
 
+    if (!body.username) {
+        response
+            .status(400)
+            .json({error: 'username is missing'});
+        return;
+    }
+
+    if (!body.password) {
+        response
+            .status(400)
+            .json({error: 'password is missing'});
+        return;
+    }
+
+    if ((body.password.length < 4) && (body.password)) {
+        return response
+            .status(400)
+            .json({error: 'password should be at least 3 character'})
+    }
+
+    if (body.username.length < 4) {
+        response
+            .status(400)
+            .json({error: 'username must be at least 3 characters long'});
+        return;
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
