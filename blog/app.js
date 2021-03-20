56c6e3c2d34809570f73c038f3ec01baf78f70d6
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-20 11:59:11 +0600

    for 5th module

--- blog/app.js
+++ blog/app.js
@@ -22,11 +22,19 @@ mongoose
         logger.error("Error connecting to db", err.message);
     });
 
+
+
 app.use(cors())
 app.use(express.json())
 app.use(middleware.tokenExtractor)
 app.use('/api/login', loginRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/blogs", blogsRouter)
+
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing');
+    app.use('/api/testing', testingRouter);
+ }
+
 
 module.exports = app
--- blog/utils/config.js
+++ blog/utils/config.js
@@ -1,17 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT
-const JWT_SECRET = process.env
-const SECRET = process.env
-let MONGODB_URI = process.env.MONGODB_URI
+let { PORT, MONGODB_URI, JWT_SECRET } = process.env;
 
 if (process.env.NODE_ENV === 'test') {
-  MONGODB_URI = process.env.TEST_MONGODB_URI
+  MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
 
 module.exports = {
   MONGODB_URI,
   PORT,
   JWT_SECRET,
-  SECRET
-}
+};
